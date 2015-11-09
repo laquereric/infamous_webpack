9c303f2528e9a9637c1a9ccd1bdfc2e31ba00898
Eric Laquer <LaquerEric@gmail.com>
2015-11-09 11:00:30 -0500

    After: following directions from https://github.com/infamous/infamous

---
+++ src/app.js
@@ -0,0 +1,18 @@
+tyle                  = require('famous/core/famous.css') // needed by famous
+var Plane                  = require('infamous/Plane')
+var contextWithPerspective = require('infamous/utils').contextWithPerspective
+
+var ctx = contextWithPerspective(1000)
+var square = new Plane({
+    size: [200,200],
+    content: 'Hello.',
+    properties: {
+        backfaceVisibility: 'visible',
+        background: 'pink',
+        padding: '5px'
+    }
+})
+
+ctx.add(square)
+square.transform.setRotate([0,2*Math.PI,0], {duration: 5000, curve: 'easeInOut'})
+
