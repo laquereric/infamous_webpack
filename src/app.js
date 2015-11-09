04c4fcc2956815d987d09048c5e6ab2abee799a2
Eric Laquer <LaquerEric@gmail.com>
2015-11-09 16:05:26 -0500

    Added semicolons

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-tyle                  = require('famous/core/famous.css') // needed by famous
-var Plane                  = require('infamous/Plane')
-var contextWithPerspective = require('infamous/utils').contextWithPerspective
+//var style                  = require('famous/core/famous.css') // needed by famous
+var Plane                  = require('infamous/Plane');
+var contextWithPerspective = require('infamous/utils').contextWithPerspective;
 
-var ctx = contextWithPerspective(1000)
+var ctx = contextWithPerspective(1000);
 var square = new Plane({
     size: [200,200],
     content: 'Hello.',
@@ -11,8 +11,7 @@ var square = new Plane({
         background: 'pink',
         padding: '5px'
     }
-})
+});
 
-ctx.add(square)
-square.transform.setRotate([0,2*Math.PI,0], {duration: 5000, curve: 'easeInOut'})
-
+ctx.add(square);
+square.transform.setRotate([0,2*Math.PI,0], {duration: 5000, curve: 'easeInOut'});
